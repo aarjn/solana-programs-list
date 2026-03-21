4900a98ef704c66381c55b75a3cc1f7df8a79287
4rjunc <arjunmundamani@gmail.com>
2026-03-21 13:32:39 +0530

    feat: add a simple quasar prgoram

---
+++ quasar_hello_world/src/lib.rs
@@ -0,0 +1,31 @@
+#![cfg_attr(not(test), no_std)]
+
+use quasar_lang::prelude::*;
+
+declare_id!("4qm3iMXJFdPh8uH3uiWiDj9bBYUHaP5D1FGKQCD87kez");
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    pub payer: &'info mut Signer,
+    pub system_program: &'info Program<System>,
+}
+
+impl<'info> Initialize<'info> {
+    #[inline(always)]
+    pub fn initialize(&self) -> Result<(), ProgramError> {
+        Ok(())
+    }
+}
+
+#[program]
+mod quasar_hello_world {
+    use super::*;
+
+    #[instruction(discriminator = 0)]
+    pub fn initialize(ctx: Ctx<Initialize>) -> Result<(), ProgramError> {
+        ctx.accounts.initialize()
+    }
+}
+
+#[cfg(test)]
+mod tests;
