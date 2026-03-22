30769c3fbe15fb53ea9d5a793e582bde03fff6ca
4rjunc <arjunmundamani@gmail.com>
2026-03-22 15:03:56 +0530

    feat: add increment

--- quasar-counter/src/instructions/increment.rs
+++ quasar-counter/src/instructions/increment.rs
@@ -1,14 +1,18 @@
+use crate::states::Counter;
 use quasar_lang::prelude::*;
 
 #[derive(Accounts)]
 pub struct Increment<'info> {
     pub payer: &'info mut Signer,
+    #[account(mut, seeds = [b"counter"], bump = counter.bump)]
+    pub counter: &'info mut Account<Counter>,
     pub system_program: &'info Program<System>,
 }
 
 impl<'info> Increment<'info> {
     #[inline(always)]
-    pub fn increment(&self) -> Result<(), ProgramError> {
+    pub fn increment(&mut self) -> Result<(), ProgramError> {
+        self.counter.count += 1;
         Ok(())
     }
 }
--- quasar-counter/src/tests.rs
+++ quasar-counter/src/tests.rs
@@ -13,12 +13,13 @@ fn setup() -> QuasarSvm {
 }
 
 #[test]
-fn test_initialize() {
+fn test_flow() {
     let mut svm = setup();
 
     let payer = Pubkey::new_unique();
     let (counter, counter_bump) = Pubkey::find_program_address(&[b"counter"], &crate::ID);
 
+    // Initialize Instruction
     let instruction: Instruction = InitializeInstruction {
         payer: Address::from(payer.to_bytes()),
         counter: Address::from(counter.to_bytes()),
@@ -52,6 +53,35 @@ fn test_initialize() {
     let count = u64::from_le_bytes(data[1..9].try_into().unwrap());
     let bump = data[9];
     assert_eq!(count, 0);
+    assert_eq!(bump, counter_bump);
+
+    result.assert_success();
+
+    // Increament Instruction
+    let incrment_instruction: Instruction = IncrementInstruction {
+        payer: Address::from(payer.to_bytes()),
+        counter: Address::from(counter.to_bytes()),
+        system_program: Address::from(quasar_svm::system_program::ID.to_bytes()),
+    }
+    .into();
+
+    let result = svm.process_instruction(
+        &incrment_instruction,
+        &[Account {
+            address: payer,
+            lamports: 10_000_000_000,
+            data: vec![],
+            owner: quasar_svm::system_program::ID,
+            executable: false,
+        }],
+    );
+
+    let counter_account = svm.get_account(&counter).unwrap();
+
+    let data = &counter_account.data;
+    let count = u64::from_le_bytes(data[1..9].try_into().unwrap());
+    let bump = data[9];
+    assert_eq!(count, 1);
     assert_eq!(bump, counter_bump);
 
     result.assert_success();
